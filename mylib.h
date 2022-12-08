af3a5a25d889fa89e694dc34fd22e89d62226f47
João Victor Godinho W <joaovgodinho@aluno.puc-rio.br>
2022-12-08 18:35:51 -0300

    Update mylib.h

---
+++ mylib.h
@@ -0,0 +1,4 @@
+struct linkedlist typedef LinkedList;
+LinkedList* create_ll(void);
+void insert_end_ll(LinkedList *node, LinkedList *to_insert);
+void insert_ini_ll(LinkedList *node, LinkedList *to_insert);
